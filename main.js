1b25a9e7f60b624cba08cbda1b4874418964633f
martinmt-mx <mmojica0@ucol.mx>
2020-09-29 23:19:52 -0500

    Descuento aplicado

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ console.log(" 1201 a 1400   :: 10%")
 console.log(" 1401 a 1500   :: 8%")
 console.log(" mas de 1500   :: 5%")
 console.log("-------------------------")
+var sueldo1 = 500
+var sueldoTotal
+
+if (sueldo1>=0 && sueldo1<=1000){
+    sueldoTotal = sueldo1*1.15
+}
+else if(sueldo1>1001 && sueldo1<1200){
+    sueldoTotal = sueldo1*1.12
+}
+else if(sueldo1>1201 && sueldo1<1400){
+    sueldoTotal = sueldo1*1.10
+}
+else if(sueldo1>1401 && sueldo1<1500){
+    sueldoTotal = sueldo1*1.08
+}
+else if(sueldo1>1500){
+    sueldoTotal = sueldo1*1.05
+}
+
+console.log("Sueldo Original:", sueldo1)
+console.log("Sueldo Aumentado:", sueldoTotal)
